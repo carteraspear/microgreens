c59a789f4eb1d01aa557bbbdf51b1f2d002a2539
Carter <carterspear@proton.me>
2025-01-09 18:51:05 -0600

    removing the double bars
    this shouuuld get rid of the both bars

--- src/main.tsx
+++ src/main.tsx
@@ -13,24 +13,9 @@ const MicrogreensApp = () => {
   return (
     <React.StrictMode>
       <div id="app-container">
-        <div className="top-bar">
-          <div className="profile-picture"></div>
-          <div className="search-field">
-            <span className="icon">🔍</span>
-            <input type="text" placeholder="Search and filter…" />
-          </div>
-          <div className="hamburger-menu"></div>
-        </div>
         <main>
           <App />
         </main>
-        <div className="green-circle">↓</div>
-        <div className="bottom-bar">
-          <div className="tab">Neighbors</div>
-          <div className="tab">Posts</div>
-          <div className="tab">Favorites</div>
-          <div className="tab">Messages</div>
-        </div>
       </div>
     </React.StrictMode>
   );
